769e713668dd8a47978b8bfa8d16b2662e3a4b6c
Anthony Gutierrez <anthony.gutierrez@pucp.pe>
2017-10-17 17:31:07 -0500

    Menú, editor, insertar objetos

---
+++ Source/EditorVR/EditorVRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EditorVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDITORVR_API AEditorVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
